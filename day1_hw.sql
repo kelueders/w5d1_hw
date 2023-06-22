5fca2592f114b6d59b2d9b81d89ca70c2d3662ad
Kate Lueders <kelueders@gmail.com>
2023-06-21 20:11:26 -0500

    corrections

--- day1_hw.sql
+++ day1_hw.sql
@@ -5,6 +5,7 @@ SELECT * FROM customer;
 SELECT * FROM payment;
 SELECT * FROM address;
 SELECT * FROM film_actor;
+SELECT * FROM rental;
 
 -- Question #1
 SELECT COUNT(last_name) FROM actor
@@ -25,8 +26,9 @@ SELECT COUNT(last_name) FROM customer
 WHERE last_name = 'William';
 
 -- Question #5
-SELECT COUNT(staff_id) FROM payment
-GROUP BY staff_id;
+SELECT COUNT(staff_id) FROM rental
+GROUP BY staff_id
+LIMIT 1;
 
 -- Question #6
 SELECT COUNT(DISTINCT district) FROM address;
@@ -39,7 +41,7 @@ LIMIT 1;
 
 -- Question #8
 SELECT last_name FROM customer
-WHERE last_name LIKE '%es';
+WHERE last_name LIKE '%es' and store_id=1;
 
 -- Question #9
 SELECT amount, COUNT(amount) FROM payment
@@ -49,4 +51,4 @@ HAVING COUNT(amount) > 250;
 
 -- Question #10
 SELECT rating, COUNT(rating) FROM film
-GROUP BY rating;
+GROUP BY rating
